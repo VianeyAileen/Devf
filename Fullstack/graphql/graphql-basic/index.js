483e865c56a9a6223506a8166c561f3c27989d8a
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-11-30 20:54:16 -0600

    add basic queries to the schema

--- Fullstack/graphql/graphql-basic/index.js
+++ Fullstack/graphql/graphql-basic/index.js
@@ -3,21 +3,40 @@ const app = express()
 
 const {buildSchema} = require ('graphql')
 const {graphqlHTTP} = require ('express-graphql')
+const {courses} = require ('./data.json')
 
 // Cosntruimos el schema
 const schema = buildSchema(`
    type Query {
     getWelcome: String 
+    getName(name: String, age: Int): String
+    getCourses: [Course]
    } 
+
+   type Course {
+    id: Int
+    title: String
+    description: String
+   }
 `)
 
 const getWelcome = () => {
     return 'Hola mundo' //porque el schema regresa un String
 }
 
+const getName = (args) => {
+    return 'Hello ' + args.name + ' ' + args.age
+}
+
+const getCourses = () => {
+    return courses
+}
+
 const root = {
     //properties que tiene el (Schema): functions que resuleve el query/mutations
-    getWelcome: getWelcome
+    getWelcome: getWelcome,
+    getName: getName,
+    getCourses: getCourses
 }
 
 app.use('/graphql', graphqlHTTP({
