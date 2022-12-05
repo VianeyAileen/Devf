f1cec04ae18c463c2cef9d77326e46d21fa152b1
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-12-05 17:40:48 -0600

    Queries and Mutations done

--- Fullstack/graphql/graphql-basic/index.js
+++ Fullstack/graphql/graphql-basic/index.js
@@ -11,7 +11,14 @@ const schema = buildSchema(`
     getWelcome: String 
     getName(name: String, age: Int): String
     getCourses: [Course]
+    getCourse(id: Int): Course 
    } 
+
+   type Mutation {
+    addCourse(id: Int!, title: String!, description: String): [Course]
+    updateCourse(id: Int, title: String, description: String): Course
+    removeCourse(id: Int): [Course]
+   }
 
    type Course {
     id: Int
@@ -32,11 +39,51 @@ const getCourses = () => {
     return courses
 }
 
+const getCourse = ({id}) => {
+    // let getIndex = courses.findIndex(index => index.id == id)
+    return courses.find(index => index.id == id)
+}
+
+const addCourse = ({id, title, description}) => {
+    // create object
+    const newCourse = {id: id, title: title, description: description}
+    // push the object to []
+    courses.push(newCourse)
+    // return the collection []
+    return courses
+}
+
+const updateCourse = ({id, title, description}) => {
+    // Find the course
+    courses.map(course => {
+        if (course.id == id) {
+            course.title = title ? title : course.title
+            course.description = description ? description : course.description
+        }
+        return course
+    })
+    // Return object by id
+    return courses.find(course => course.id == id)
+}
+
+const removeCourse = ({id}) => {
+    // Find the course to remove
+    let getIndex = courses.findIndex(index => index.id == id)
+    // Remove the course
+    courses.splice(getIndex, 1)
+    // Return courses without the removed course
+    return courses
+}
+
 const root = {
     //properties que tiene el (Schema): functions que resuleve el query/mutations
     getWelcome: getWelcome,
     getName: getName,
-    getCourses: getCourses
+    getCourses: getCourses,
+    addCourse: addCourse,
+    updateCourse: updateCourse,
+    removeCourse: removeCourse,
+    getCourse: getCourse
 }
 
 app.use('/graphql', graphqlHTTP({
